d8ee7635ca466d0e6b48f9fa843410ef1bed9380
somsakPanta <soms2819@gmail.com>
2016-02-06 13:32:48 +0700

    Test insert and delete data

--- app/src/main/java/com/sp/sprestaurant/MainActivity.java
+++ app/src/main/java/com/sp/sprestaurant/MainActivity.java
@@ -1,15 +1,56 @@
 package com.sp.sprestaurant;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    //Step 1.
+    private MyManage objMyManage;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Step 2.
+        //Request database
+        objMyManage = new MyManage(this);
+
+
+        //Test Add value
+        //testAddValue();
+
+        //Clean data
+        cleanData();
+
 
     } //Main Method
+
+    private void cleanData() {
+        SQLiteDatabase objSqLiteDatabase = openOrCreateDatabase(MyOpenHelper.database_name,
+                MODE_PRIVATE,null);
+        //MODE_PRIVATE อนุญาติให้ลบข้อมูล แต่ไม่อนุญาติให้ Drop table
+
+        objSqLiteDatabase.delete(MyManage.food_TABLE, null, null);
+        objSqLiteDatabase.delete(MyManage.user_TABLE, null, null);
+
+
+    }// cleanData
+
+    private void testAddValue() {
+        for(int i=0;i<=1;i++) {
+
+            objMyManage.addNewValue(i, "test1", "test2", "test3");
+
+
+        }// for
+
+
+    }// testAddValue
+
+
 }// Main class
--- app/src/main/java/com/sp/sprestaurant/MyManage.java
+++ app/src/main/java/com/sp/sprestaurant/MyManage.java
@@ -1,5 +1,6 @@
 package com.sp.sprestaurant;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
@@ -12,6 +13,18 @@ public class MyManage {
     private MyOpenHelper objMyOpenHelper;
     private SQLiteDatabase writeSqLiteDatabase, readSqLiteDatabase;
 
+    public static final String user_TABLE = "userTABLE";
+    public static final String food_TABLE = "foodTABLE";
+
+    public static final String column_id = "_id";
+    public static final String column_user = "User";
+    public static final String column_password = "Password";
+    public static final String column_name = "Name";
+
+    public static final String column_food = "Food";
+    public static final String column_price = "Price";
+    public static final String column_source = "Source";
+
 
     public MyManage(Context context) {
 
@@ -21,5 +34,43 @@ public class MyManage {
         readSqLiteDatabase = objMyOpenHelper.getReadableDatabase();
 
     }// Constructor
+
+    //Step 3.
+    //Add method
+    public long addNewValue(int intTABLE,
+                            String strColumn2,
+                            String strColumn3,
+                            String strColumn4) {
+
+        ContentValues objContentValues = new ContentValues();
+
+        long longReturn = 0;
+
+
+        switch (intTABLE) {
+
+            case 0:
+
+                objContentValues.put(column_user,strColumn2);
+                objContentValues.put(column_password,strColumn3);
+                objContentValues.put(column_name,strColumn4);
+                //Add data to table user
+                writeSqLiteDatabase.insert(user_TABLE, null, objContentValues);
+
+
+                break;
+            case 1:
+
+                objContentValues.put(column_food,strColumn2);
+                objContentValues.put(column_price,strColumn3);
+                objContentValues.put(column_source,strColumn4);
+                //Add data to table user
+                writeSqLiteDatabase.insert(food_TABLE, null, objContentValues);
+
+                break;
+        } //switch
+
+        return longReturn;
+    }
 
 }//Main class
